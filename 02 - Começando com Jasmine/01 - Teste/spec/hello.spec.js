d205d7477d3b262d4a76ee3c9c4d403f817e96c8
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-06 01:52:27 -0300

    fi: up project

--- 02 - Começando com Jasmine/01 - Teste/spec/hello.spec.js
+++ 02 - Começando com Jasmine/01 - Teste/spec/hello.spec.js
@@ -1,7 +1,7 @@
 var hello = require("../hello");
 describe("Hello", function () {
-  it('has to print "Hello World!"', function () {
-    var text = hello();
-    expect(text).toEqual("Hello World!");
-  });
+    it('has to print "Hello World!"', function () {
+        var text = hello();
+        expect(text).toEqual("Hello World!");
+    });
 });
--- 03 - Matchers/01 - Create Matcher/spec/scripts.spec.js
+++ 03 - Matchers/01 - Create Matcher/spec/scripts.spec.js
@@ -1,28 +1,28 @@
 var isAnagram = require("../scripts");
 
 describe("Anagram", function () {
-  var myMatchers = {
-    toBe2: function (util, customEqualityTesters) {
-      return {
-        compare: function (actual, expected) {
-          var result = {};
-          result.pass = actual === 2;
+    var myMatchers = {
+        toBe2: function (util, customEqualityTesters) {
+            return {
+                compare: function (actual, expected) {
+                    var result = {};
+                    result.pass = actual === 2;
 
-          if (!result.pass) {
-            result.message = "Expected " + actual + " to be exactly 2";
-          }
+                    if (!result.pass) {
+                        result.message = "Expected " + actual + " to be exactly 2";
+                    }
 
-          return result;
+                    return result;
+                },
+            };
         },
-      };
-    },
-  };
+    };
 
-  beforeEach(function () {
-    jasmine.addMatchers(myMatchers);
-  });
+    beforeEach(function () {
+        jasmine.addMatchers(myMatchers);
+    });
 
-  it("is 2", function () {
-    expect(2).toBe2();
-  });
+    it("is 2", function () {
+        expect(2).toBe2();
+    });
 });
